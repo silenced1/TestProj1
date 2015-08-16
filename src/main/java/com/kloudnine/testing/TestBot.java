c888cbcc16716fe9a3deb7f02764b44290cbbba9
SiLeNCeD <silenced@kloudnine.com>
2015-08-16 16:39:51 -0400

    First Commit?
    Maybe this is the first

---
+++ src/main/java/com/kloudnine/testing/TestBot.java
@@ -0,0 +1,20 @@
+package com.kloudnine.testing;
+
+/**
+ * Created by Steven on 8/16/2015.
+ */
+public class TestBot {
+
+    public static void main(String[] args) {
+
+        String greeting = new String();
+        greeting = "Hello";
+
+        System.out.println(greeting);
+
+
+    }
+
+
+
+}
